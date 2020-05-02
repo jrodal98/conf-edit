3aa2434d7041ff75b2b0830ded769af6f7faeaf6
Jacob Rodal <jrodal98@gmail.com>
2020-05-01 20:51:44 -0400

    reading/creating config and ls implemented

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    fs::{File, OpenOptions},
+    io::{Seek, SeekFrom},
+    path::{Path, PathBuf},
+};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -14,7 +20,7 @@ pub struct Add {
 }
 
 impl Add {
-    fn run(&self) {
+    fn run(&self, conf_file: File) {
         dbg!("Add");
     }
 }
@@ -27,7 +33,7 @@ pub struct Remove {
 }
 
 impl Remove {
-    fn run(&self) {
+    fn run(&self, conf_file: File) {
         dbg!("remove");
     }
 }
@@ -40,7 +46,7 @@ pub struct Execute {
 }
 
 impl Execute {
-    fn run(&self) {
+    fn run(&self, conf_file: File) {
         dbg!("execute");
     }
 }
@@ -55,7 +61,7 @@ pub struct Edit {
 }
 
 impl Edit {
-    fn run(&self) {
+    fn run(&self, conf_file: File) {
         dbg!("edit");
     }
 }
@@ -68,8 +74,11 @@ pub struct ListFiles {
 }
 
 impl ListFiles {
-    fn run(&self) {
+    fn run(&self, conf_file: File) {
         dbg!("ls");
+        let json_text: JsonFile =
+            serde_json::from_reader(&conf_file).expect("error opening config file");
+        println!("{:?}", json_text);
     }
 }
 
@@ -91,14 +100,68 @@ pub enum Command {
 
 impl Command {
     fn run(&self) {
+        let conf_file = self.open_config();
         match self {
-            Command::Add(command) => command.run(),
-            Command::Remove(command) => command.run(),
-            Command::Execute(command) => command.run(),
-            Command::Edit(command) => command.run(),
-            Command::ListFiles(command) => command.run(),
+            Command::Add(command) => command.run(conf_file),
+            Command::Remove(command) => command.run(conf_file),
+            Command::Execute(command) => command.run(conf_file),
+            Command::Edit(command) => command.run(conf_file),
+            Command::ListFiles(command) => command.run(conf_file),
         }
     }
+
+    fn open_config(&self) -> File {
+        let path = dirs::home_dir()
+            .expect("ERROR: $HOME is not set.")
+            .join(".config/conf-edit/config");
+        if Path::exists(&path) {
+            OpenOptions::new()
+                .read(true)
+                .write(true)
+                .open(path)
+                .expect("Error opening config file")
+        } else {
+            let editor = match std::env::var("EDITOR") {
+                Ok(val) => val,
+                Err(_) => "vim".to_string(),
+            };
+
+            let entry = Entry {
+                path: path.to_str().unwrap().to_owned(),
+                editor: editor.clone(),
+                hook: "echo 'conf-edit config edited!'".to_owned(),
+            };
+
+            let mut data = HashMap::new();
+            data.insert("conf-edit".to_owned(), entry);
+
+            let f = JsonFile { editor, data };
+
+            let mut file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .open(path)
+                .expect("Error opening and/or creating config file");
+            serde_json::to_writer(&file, &f).expect("Error writing to config");
+            file.seek(SeekFrom::Start(0))
+                .expect("Error rewinding file pointer"); // rewind file pointer back to beginning
+            file
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Entry {
+    path: String,
+    editor: String,
+    hook: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct JsonFile {
+    editor: String,
+    data: HashMap<String, Entry>,
 }
 
 fn run_app() -> Result<(), ()> {
